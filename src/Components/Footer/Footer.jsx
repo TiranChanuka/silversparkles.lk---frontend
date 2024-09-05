8ba5cab108363bd4b80380162b8c922903205793
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-09-05 14:41:59 +0530

    change footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,39 +9,46 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import PinterestIcon from "@mui/icons-material/Pinterest";
 import { Link, NavLink } from "react-router-dom";
-import LogoWhite from "../../assets/logoB.png";
+import LogoWhite from "../../assets/logo/logob.png";
+import JemLogo from "../../assets/logo/jemlogo.png"
+import { FaTiktok } from "react-icons/fa6";
+
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="footer">
+    <div className="footer pt-10">
       {/* contents */}
-      <div className="flex flex-col gap-[50px] ">
-        {/* section 1 */}
-        <div>
-          <div className="flex flex-col md:flex-row gap-[20px] items-center px-[100px] pt-[20px]">
-            <NavLink to={"/"} className="cursor-pointer">
-              <img src={LogoWhite} alt="Logo" className=" w-[100%]" />
-            </NavLink>
-          </div>
-        </div>
+      <div className="flex flex-col gap-[10px] ">
+
 
         {/* section 2 */}
 
 
         {/* section 2 */}
         <div className="flex flex-col md:flex-row gap-[40px] md:gap-0 items-center md:items-start text-center md:text-left  md:justify-between px-[100px]">
+
           {/*item 1  */}
           <div>
             <div className="Outfit flex flex-col text-black font-[500] gap-[5px]">
+              {/* section 1 */}
+              <div>
+                <div className="flex flex-col md:flex-row gap-[20px] items-center">
+                  <NavLink to={"/"} className="cursor-pointer">
+                    <img src={LogoWhite} width={250} alt="Logo" />
+                  </NavLink>
+                </div>
+              </div>
               <div className="flex flex-col">
-                <span>1 GALLEFACE,</span>
-                <span>COLOMBO 2,</span>
-                <span>SRI LANKA,</span>
+                <span>No 30,  </span>
+                <span>Dr Lester James Peiris Mawatha,</span>
+                <span>Colombo 5,</span>
+                <span>Sri Lanka,</span>
               </div>
 
               <div className="flex flex-col">
-                <span>011 2345 678</span>
+                <span>0778884033</span>
+                <span>0112555515</span>
                 <span>contact@goldhouse.com</span>
               </div>
             </div>
@@ -73,48 +80,86 @@ const Footer = () => {
             <div className="flex flex-col">
               {/* topic */}
               <h1 className="Poppins text-2xl font-bold text-black mb-2">
+                Information
+              </h1>
+              <div className="Outfit flex flex-col text-black gap-[10px]">
+                <span className="linkFooter">SS Bling Gang</span>
+                <Link to={"/about"} className="linkFooter">About Us</Link>
+                <span className="linkFooter">Custom order</span>
+                <span className="linkFooter">Membership</span>
+              </div>
+            </div>
+          </div>
+
+          {/* item 4 */}
+          <div>
+            <div className="flex flex-col">
+              {/* topic */}
+              <h1 className="Poppins text-2xl font-bold text-black mb-2">
                 Help
               </h1>
               <div className="Outfit flex flex-col text-black gap-[10px]">
-                <span className="linkFooter">Bracelets Size Guides</span>
-                <span className="linkFooter">Ring Size Guides</span>
-                <span className="linkFooter">Chain Size Guides</span>
-                <span className="linkFooter">Initiate A Return</span>
+                <Link to={"/contact-us"} className="linkFooter">
+                  Contact Us
+                </Link>
+                <span className="linkFooter">Return Policy</span>
                 <span className="linkFooter">Warranty</span>
+                <span className="linkFooter">Ring Size Guide</span>
+                <span className="linkFooter">Bracelet Size Guide </span>
+                <span className="linkFooter">Chain Size Guide</span>
                 <Link to={"/terms-of-services"} className="linkFooter">
                   Terms Of Services
                 </Link>
                 <Link to={"/privacy-policy"} className="linkFooter">
                   Privacy Policy
                 </Link>
-                <Link to={"/contact-us"} className="linkFooter">
-                  Contact Us
-                </Link>
-                <Link to={"/about"} className="linkFooter">About Us</Link>
+
+
               </div>
             </div>
           </div>
-        </div>
 
+          {/* item 4 */}
+          <div>
+            <div className="flex flex-col">
+              {/* topic */}
+              <h1 className="Poppins text-2xl font-bold text-black mb-2">
+                Shop With Us
+              </h1>
+              <div className="Outfit text-black  space-y-5">
+                <div className="flex justify-between">
+                  <img src={PostPay} alt="postpay" className="w-[50px]" />
+                  <img src={WireCard} alt="wirecard" className="w-[50px]" />
+                </div>
+                <div className="flex justify-between">
+                  <img src={Worldpay} alt="worldpay" className="w-[50px]" />
+                  <img src={Unionpay} alt="unionpay" className="w-[50px]" />
+                </div>
+                <div className="flex justify-between">
+                  <img src={Cr} alt="cr" className="w-[50px] bg-white rounded-full" />
+                  <img src={Maestro} alt="ma" className="w-[50px] rounded-full" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
         {/* section 3 */}
-        <div className="flex flex-col md:flex-row items-center gap-[100px] px-[100px]">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-[10px] px-[100px]">
           {/* left */}
-          <div className="Poppins text-[30px] md:text-left text-center text-black font-[700]">
-            <h1>Shop With Us</h1>
+          <div className="Poppins text-[20px] md:text-left text-center text-black font-[700]">
+            <h1>SLGJA Authorized </h1>
           </div>
           {/* right */}
-          <div className="flex gap-[50px] flex-wrap items-center justify-center">
-            <img src={PostPay} alt="postpay" className="w-[50px]" />
-            <img src={WireCard} alt="wirecard" className="w-[50px]" />
-            <img src={Worldpay} alt="worldpay" className="w-[50px]" />
-            <img src={Unionpay} alt="unionpay" className="w-[50px]" />
-            <img src={Cr} alt="cr" className="w-[50px] bg-white rounded-full" />
-            <img src={Maestro} alt="ma" className="w-[50px] rounded-full" />
-          </div>
+          <img src={JemLogo} alt="postpay" className="w-[100px]" />
         </div>
 
-        {/* section 4 */}
-        <div className="flex flex-col md:flex-row px-[100px] gap-[20px] text-black">
+
+
+
+        {/* section 5 */}
+        <div className="flex flex-col md:flex-row px-[100px] gap-[20px] text-black justify-center">
           {/* left */}
           <div className="Poppins text-[18px] text-center md:text-left font-[500]">
             <h1>Follow us</h1>
@@ -124,12 +169,12 @@ const Footer = () => {
             <FacebookIcon />
             <InstagramIcon />
             <YouTubeIcon />
-            <PinterestIcon />
+            <FaTiktok size={20} />
           </div>
         </div>
 
-        {/* section 5 */}
-        <div className="Outfit bg-[#0a6550] p-2 flex text-white justify-center items-center text-sm">
+        {/* section 6 */}
+        <div className="Outfit bg-[#0a6550] p-4 flex text-white justify-center items-center text-sm mt-5">
           <div>
             Copyright © {currentYear} Silver Sparkles.
             Developed By{" "}
