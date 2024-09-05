ac70404bce24667e74f751e66c086e804f57d8c5
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-09-05 14:41:42 +0530

    create slider

--- src/Components/Client Card/ClientCard.jsx
+++ src/Components/Client Card/ClientCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const ClientCard = ({item}) => {
+const ClientCard = ({ item }) => {
   return (
     <div>
       <div className='Poppins w-[250px] h-[500px] flex relative'>
         {/* client Image */}
         <img src={item.img} alt="client" className='w-[100%] h-[100%] object-cover' />
         {/* client Name */}
-        <div className='text-white absolute bottom-0 p-[10px]'>
-          <span className='text-[20px] '>{item.name}</span>
+        <div className='text-white absolute bottom-0 p-2 bg-[#0A6550] bg-opacity-90 w-full' >
+          <div><span className='text-[20px] font-semibold'>{item.name}</span></div>
+          <span className='text-[16px]' >{item.description}</span>
         </div>
       </div>
     </div>
